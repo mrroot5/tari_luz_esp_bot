29981d417fc1694079b4b77b87477b45b6c6baba
Adrián <1659540+mrroot5@users.noreply.github.com>
2024-07-20 20:05:13 +0200

    Fix code style (#2)
    * Moved private functions down
    
    * Added docstring
    
    * Fix pylint errors and added config file
    
    * Deleted old settings file
    
    * Added webhook url config with sample url
    
    * Added private chat regular text reply

--- bot.py
+++ bot.py
@@ -29,6 +29,142 @@ logging.basicConfig(
 logger = logging.getLogger('tari_luz_bot')
 
 
+async def command_cheapest(update: Update, _context: ContextTypes.DEFAULT_TYPE):
+    """
+    Handles the /cheapest command, retrieves the current date, reads data from a CSV file,
+    updates the cache file if needed, and sends a message with the cheapest price data in HTML format.
+
+    Parameters:
+        update (Update): The update object containing information about the incoming message.
+        _context (ContextTypes.DEFAULT_TYPE): Context object containing information about the current state of the bot.
+    """
+    logger.info('Bot asked to execute /cheapest command')
+    utc_now = datetime.datetime.now(datetime.UTC)
+    date_str = utc_now.strftime('%d/%m/%Y')
+    msg = []
+
+    try:
+        with open('today.csv', encoding="utf-8") as csvfile:
+            file_read = csv.reader(csvfile)
+            file_date = next(file_read)[0]
+            msg = [f"<pre>Hoy {file_date}</pre>"]
+
+            if file_date == date_str:
+                msg = __get_price_data_from_file(file_reader=file_read, message=msg)
+            else:
+                raise FileNotFoundError
+    except FileNotFoundError:
+        __update_cache_file_with_cheapest(date_str)
+
+        with open('today.csv', encoding="utf-8") as csvfile:
+            file_read = csv.reader(csvfile)
+            file_date = next(file_read)[0]
+            msg = [f"<pre>Hoy {file_date}</pre>"]  # ignore first line
+            msg = __get_price_data_from_file(file_reader=file_read, message=msg)
+
+    await update.message.reply_text(
+        f"{linesep}".join(msg),
+        parse_mode=telegram.constants.ParseMode('HTML')
+    )
+
+
+async def command_help(update: Update, _context: ContextTypes.DEFAULT_TYPE):
+    """
+    Handles the /help command.
+
+    It sends a message with a list of available commands to the user.
+
+    Parameters:
+        update (Update): The update object containing information about the incoming message.
+        _context (ContextTypes.DEFAULT_TYPE): Context object containing information about the current state of the bot.
+
+    Returns:
+        None
+    """
+    logger.info('Received command /help')
+    await update.message.reply_text(
+        "Comandos disponibles:\n"
+        " - /start - Comienza a interactuar con el bot\n"
+        " - /help - Mostrar las interacciones disponibles\n"
+        " - /status - Mostrar el estado del bot\n"
+        " - /cheapest - Obtener el precio mas barato de la luz"
+    )
+
+
+async def command_start(update: Update, _context: ContextTypes.DEFAULT_TYPE):
+    """
+    Handles the /start command. It replies with the bot greeting message.
+
+    Parameters:
+        update (Update): The update object containing information about the incoming message.
+        _context (ContextTypes.DEFAULT_TYPE): Context object containing information about the current state of the bot.
+
+    Returns:
+        None
+    """
+    logger.info('Received command /start')
+    await update.message.reply_text(config.BOT_GREETING)
+
+
+async def command_status(update: Update, _context: ContextTypes.DEFAULT_TYPE):
+    """
+    Handles the /status command. It replies with the bot uptime.
+
+    Parameters:
+        update (Update): The update object containing information about the incoming message.
+        _context (ContextTypes.DEFAULT_TYPE): Context object containing information about the current state of the bot.
+
+    Returns:
+        None
+    """
+    logger.info('bot asked to execute /status command')
+    await update.message.reply_text(f'Status is OK, running since {utils.since()}')
+
+
+async def handle_private_message(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> None:
+    """
+    Just tell the use to use commands instead of regular text.
+
+    Args:
+        update (Update): The update object containing information about the incoming message.
+        _context (ContextTypes.DEFAULT_TYPE): Context object containing information about the current state of the bot.
+
+    Returns:
+        None
+    """
+    await update.message.reply_text('I would be glad to have a conversation with you but I only reply to commands,\
+                                    use my /help command to know the options please')
+
+
+def main():
+    """
+    A function to initialize the main bot functionality and bot commands.
+    """
+    logging.basicConfig(
+        level=config.LOG_LEVEL,
+        format='%(asctime)s [%(name)s] %(levelname)s: %(message)s',
+    )
+    logger.info('Starting bot...')
+    config.log(logger.info)
+    app = ApplicationBuilder().token(config.TELEGRAM_BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler('start', command_start))
+    app.add_handler(CommandHandler('help', command_help))
+    app.add_handler(CommandHandler('status', command_status))
+    app.add_handler(CommandHandler('cheapest', command_cheapest))
+    app.add_handler(MessageHandler(
+        filters.TEXT & ~filters.COMMAND & filters.ChatType.PRIVATE, handle_private_message)
+    )
+
+    logger.info('Bot is ready')
+    app.run_webhook(
+        listen='127.0.0.1',
+        port=8080,
+        url_path='/',
+        webhook_url=config.WEBHOOK_URL
+    )
+
+
 def __get_price(data: dict) -> str:
     price = data.get('price')
     price_kwh = float(price) / 1000 if price else ''
@@ -62,81 +198,6 @@ def __update_cache_file_with_cheapest(date_str: str) -> bool:
 
         file_write.writerows(msg_write)
     return True
-
-
-async def command_cheapest(update, _context: ContextTypes.DEFAULT_TYPE):
-    logger.info('Bot asked to execute /cheapest command')
-    utc_now = datetime.datetime.now(datetime.UTC)
-    date_str = utc_now.strftime('%d/%m/%Y')
-    msg = []
-
-    try:
-        with open('today.csv', encoding="utf-8") as csvfile:
-            file_read = csv.reader(csvfile)
-            file_date = next(file_read)[0]
-            msg = [f"<pre>Hoy {file_date}</pre>"]
-
-            if file_date == date_str:
-                msg = __get_price_data_from_file(file_reader=file_read, message=msg)
-            else:
-                raise FileNotFoundError
-    except FileNotFoundError:
-        __update_cache_file_with_cheapest(date_str)
-
-        with open('today.csv', encoding="utf-8") as csvfile:
-            file_read = csv.reader(csvfile)
-            file_date = next(file_read)[0]
-            msg = [f"<pre>Hoy {file_date}</pre>"]  # ignore first line
-            msg = __get_price_data_from_file(file_reader=file_read, message=msg)
-
-    await update.message.reply_text(
-        f"{linesep}".join(msg),
-        parse_mode=telegram.constants.ParseMode('HTML')
-    )
-
-
-async def command_help(update, _context: ContextTypes.DEFAULT_TYPE):
-    logger.info('Received command /help')
-    await update.message.reply_text(
-        "Comandos disponibles:\n"
-        " - /start - Comienza a interactuar con el bot\n"
-        " - /help - Mostrar las interacciones disponibles\n"
-        " - /status - Mostrar el estado del bot\n"
-        " - /cheapest - Obtener el precio mas barato de la luz"
-    )
-
-
-async def command_start(update, _context: ContextTypes.DEFAULT_TYPE):
-    logger.info('Received command /start')
-    await update.message.reply_text(config.BOT_GREETING)
-
-
-async def command_status(update, _context: ContextTypes.DEFAULT_TYPE):
-    logger.info('bot asked to execute /status command')
-    await update.message.reply_text(f'Status is OK, running since {utils.since()}')
-
-
-def main():
-    logging.basicConfig(
-        level=config.LOG_LEVEL,
-        format='%(asctime)s [%(name)s] %(levelname)s: %(message)s',
-    )
-    logger.info('Starting bot...')
-    config.log(logger.info)
-    app = ApplicationBuilder().token(config.TELEGRAM_BOT_TOKEN).build()
-
-    app.add_handler(CommandHandler('start', command_start))
-    app.add_handler(CommandHandler('help', command_help))
-    app.add_handler(CommandHandler('status', command_status))
-    app.add_handler(CommandHandler('cheapest', command_cheapest))
-
-    logger.info('Bot is ready')
-    app.run_webhook(
-        listen='127.0.0.1',
-        port=8080,
-        url_path='/',
-        webhook_url='https://88f3-92-177-3-22.ngrok-free.app'  # Replace with your ngrok URL
-    )
 
 
 if __name__ == "__main__":
