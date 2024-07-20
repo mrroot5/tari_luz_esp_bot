b053bf75864229179af2214239465e5e8c716a12
Adrián <1659540+mrroot5@users.noreply.github.com>
2024-07-20 19:05:01 +0200

    Upgrade python telegram bot (#1)
    * Bump python-telegram.bot to 21.4
    
    * Upgraded pipfile
    
    * Fix conflicts imports
    
    * Fix python-telegram-bot reply messages bugs
    
    * Replace old utils by forked version
    
    * Added requirements.txt and json from pipdeptree
    
    * Added logger
    
    * Changed settings
    
    - New greeting msg.
    - Requests timeout setting.
    
    * Added flake8 file
    
    * Updated gitignore
    
    ---------
    
    Forked from [pydecard bot](https://github.com/pythoncanarias/pydeckard).

--- bot.py
+++ bot.py
@@ -11,18 +11,22 @@ Command to get spanish light prices.
 import csv
 import datetime
 import logging
-import os
 from os import linesep
 
 import requests
 import telegram
 from telegram import Update
-from telegram.ext import Updater, Filters, MessageHandler, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
-import settings
+import config
 import utils
 
-logger = logging.getLogger('bot')
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger('tari_luz_bot')
 
 
 def __get_price(data: dict) -> str:
@@ -46,11 +50,11 @@ def __update_cache_file_with_cheapest(date_str: str) -> bool:
     number_of_prices = 5
     api_url = f'https://api.preciodelaluz.org/v1/prices/cheapests?zone=PCB&n={number_of_prices}'
 
-    with open('today.csv', 'w') as csvfile:
+    with open('today.csv', 'w', encoding='utf-8') as csvfile:
         file_write = csv.writer(csvfile)
         msg_write = [[date_str]]
 
-        result = requests.get(api_url)
+        result = requests.get(api_url, timeout=config.REQUESTS_DEFAULT_TIMEOUT)
         data_json = result.json()
 
         for index, element in enumerate(data_json, start=1):
@@ -60,14 +64,14 @@ def __update_cache_file_with_cheapest(date_str: str) -> bool:
     return True
 
 
-def command_cheapest(update, context):
+async def command_cheapest(update, _context: ContextTypes.DEFAULT_TYPE):
     logger.info('Bot asked to execute /cheapest command')
-    utc_now = datetime.datetime.utcnow()
+    utc_now = datetime.datetime.now(datetime.UTC)
     date_str = utc_now.strftime('%d/%m/%Y')
     msg = []
 
     try:
-        with open('today.csv') as csvfile:
+        with open('today.csv', encoding="utf-8") as csvfile:
             file_read = csv.reader(csvfile)
             file_date = next(file_read)[0]
             msg = [f"<pre>Hoy {file_date}</pre>"]
@@ -79,79 +83,59 @@ def command_cheapest(update, context):
     except FileNotFoundError:
         __update_cache_file_with_cheapest(date_str)
 
-        with open('today.csv') as csvfile:
+        with open('today.csv', encoding="utf-8") as csvfile:
             file_read = csv.reader(csvfile)
             file_date = next(file_read)[0]
             msg = [f"<pre>Hoy {file_date}</pre>"]  # ignore first line
             msg = __get_price_data_from_file(file_reader=file_read, message=msg)
 
-    context.bot.send_message(
-        chat_id=update.message.chat_id,
-        text=f"{linesep}".join(msg),
-        parse_mode=telegram.ParseMode.HTML
+    await update.message.reply_text(
+        f"{linesep}".join(msg),
+        parse_mode=telegram.constants.ParseMode('HTML')
     )
 
 
-def command_help(update, context):
+async def command_help(update, _context: ContextTypes.DEFAULT_TYPE):
     logger.info('Received command /help')
-    context.bot.send_message(
-        chat_id=update.message.chat_id,
-        text="Comandos disponibles:\n"
-             " - /start - Comienza a interactuar con el bot\n"
-             " - /help - Mostrar las interacciones disponibles\n"
-             " - /status - Mostrar el estado del bot\n"
+    await update.message.reply_text(
+        "Comandos disponibles:\n"
+        " - /start - Comienza a interactuar con el bot\n"
+        " - /help - Mostrar las interacciones disponibles\n"
+        " - /status - Mostrar el estado del bot\n"
+        " - /cheapest - Obtener el precio mas barato de la luz"
     )
 
 
-def command_start(update, context):
+async def command_start(update, _context: ContextTypes.DEFAULT_TYPE):
     logger.info('Received command /start')
-    context.bot.send_message(chat_id=update.message.chat_id, text=settings.BOT_GREETING)
+    await update.message.reply_text(config.BOT_GREETING)
 
 
-def command_status(update, context):
+async def command_status(update, _context: ContextTypes.DEFAULT_TYPE):
     logger.info('bot asked to execute /status command')
-    context.bot.send_message(
-        chat_id=update.message.chat_id,
-        text=f'Status is OK, running since {utils.since()}',
-    )
-
-
-def reply(update, context):
-    if not settings.bot_replies_enabled():
-        return
-
-    msg = update.message.text
-    reply_spec = utils.triggers_reply(msg) if msg else None
-    if reply_spec is not None:
-        logger.info(f'bot sends reply {reply_spec.reply}')
-        context.bot.send_message(
-            chat_id=update.message.chat_id,
-            text=reply_spec.reply
-        )
+    await update.message.reply_text(f'Status is OK, running since {utils.since()}')
 
 
 def main():
     logging.basicConfig(
-        level=settings.LOG_LEVEL,
+        level=config.LOG_LEVEL,
         format='%(asctime)s [%(name)s] %(levelname)s: %(message)s',
     )
     logger.info('Starting bot...')
-    updater = Updater(settings.BOT_TOKEN)
-    dp = updater.dispatcher
+    config.log(logger.info)
+    app = ApplicationBuilder().token(config.TELEGRAM_BOT_TOKEN).build()
 
-    # Commands
-    dp.add_handler(CommandHandler('start', command_start))
-    dp.add_handler(CommandHandler('help', command_help))
-    dp.add_handler(CommandHandler('status', command_status))
-    dp.add_handler(CommandHandler('cheapest', command_cheapest))
-    dp.add_handler(MessageHandler(Filters.chat_type.groups, reply))
+    app.add_handler(CommandHandler('start', command_start))
+    app.add_handler(CommandHandler('help', command_help))
+    app.add_handler(CommandHandler('status', command_status))
+    app.add_handler(CommandHandler('cheapest', command_cheapest))
 
     logger.info('Bot is ready')
-    updater.start_webhook(
-        listen="0.0.0.0",
-        port=int(os.environ.get('PORT', '8443')),
-        url_path=settings.BOT_TOKEN,
-        webhook_url="https://tari-luz-esp-bot.herokuapp.com/" + settings.BOT_TOKEN
+    app.run_webhook(
+        listen='127.0.0.1',
+        port=8080,
+        url_path='/',
+        webhook_url='https://88f3-92-177-3-22.ngrok-free.app'  # Replace with your ngrok URL
     )
 
 
---
+++ config.py
@@ -0,0 +1,121 @@
+from typing import NamedTuple, Any
+
+from prettyconf import config as _config
+
+
+_config_registry = []
+
+
+class _ConfigItem(NamedTuple):
+    name: str
+    value: Any
+    suppress_log: bool = False
+
+    def log(self, logger_method, indent=False):
+        value = "***PRIVATE***" if self.suppress_log else self.value
+        indentation = '   ' if indent else ''
+        logger_method(f"{indentation}{self.name} = {value}")
+
+
+def config(item, cast=lambda v: v, suppress_log=False, **kwargs):
+    value = _config(item, cast, **kwargs)
+    global _config_registry
+    _config_registry.append(_ConfigItem(item, value, suppress_log))
+    return value
+
+
+def log(logger_method):
+    logger_method("Bot configuration:")
+    for config_item in _config_registry:
+        config_item.log(logger_method, indent=True)
+
+
+TELEGRAM_BOT_TOKEN = config(
+    "TELEGRAM_BOT_TOKEN", default="put here the token of your bot", suppress_log=True
+)
+
+# How likely is the bot to be triggered by one of the patterns it recognises.
+# - Allowed values: A float from 0 to 1 (0 will disable bot replies)
+VERBOSITY = config("BOT_VERBOSITY", float, default=0.33)
+
+
+# Log level, default is WARNING
+LOG_LEVEL = config('LOG_LEVEL', default='WARNING')
+
+# Poll interval for telegram API request, default is 3 seconds
+POLL_INTERVAL = config('POLL_INTERVAL', int, default=3)
+
+# Bot message for start command
+BOT_GREETING = config('BOT_GREETING', default="Hola! Estoy listo para darte información sobre el\
+                      precio de la luz. Usa el comando /help para ver las acciones disponibles.")
+
+# A username longer than this will be considered non-human
+# - Allowed values: An integer larger than 1
+MAX_HUMAN_USERNAME_LENGTH = config('MAX_HUMAN_USERNAME_LENGTH', int, default=100)
+
+
+# We have found, through empiric evidence, that a large ration of Chinese
+# characters usually indicates the user is a spammer or bot.
+# This sets the maximum allowed percent of Chinese characters before
+# considering the user a bot.
+# - Allowed values: A float from 0 to 1
+MAX_CHINESE_CHARS_PERCENT = config('MAX_CHINESE_CHARS_PERCENT', float, default=0.15)
+
+
+# Delay (in seconds) to wait before sending welcome message. New users have
+# 5 minutes to solve a captcha. The default delay is 5 and a half minutes.
+WELCOME_DELAY = config('WELCOME_DELAY', int, default=330)
+
+
+def bot_replies_enabled() -> bool:
+    return VERBOSITY > 0
+
+
+THE_ZEN_OF_PYTHON = [
+    "Beautiful is better than ugly.",
+    "Explicit is better than implicit.",
+    "Simple is better than complex.",
+    "Complex is better than complicated.",
+    "Flat is better than nested.",
+    "Sparse is better than dense.",
+    "Readability counts.",
+    "Special cases aren't special enough to break the rules.",
+    "Although practicality beats purity.",
+    "Errors should never pass silently.",
+    "Unless explicitly silenced.",
+    "In the face of ambiguity, refuse the temptation to guess.",
+    "There should be one-- and preferably only one --obvious way to do it.",
+    "Although that way may not be obvious at first unless you're Dutch.",
+    "Now is better than never.",
+    "Although never is often better than *right* now.",
+    "If the implementation is hard to explain, it's a bad idea.",
+    "If the implementation is easy to explain, it may be a good idea.",
+    "Namespaces are one honking great idea -- let's do more of those!",
+]
+
+
+REPLIES = {
+    ("java",): "BIBA JABA!! ☕️",
+    ("cobol",): "BIBA KOBOL!! 💾",
+    ("javascript",): "BIBA JABAESKRIPT!! 🔮",
+    ("php",): "BIBA PEHACHEPÉ!! ⛱",
+    ("chatgpt", "gpt", "openai"): "BIBA CHATJEPETÉ!! 🤖",
+    (
+        "he visto",
+        "has visto",
+        "han visto",
+        "visteis",
+        "vieron",
+        "vi",
+    ): "Yo he visto cosas que vosotros no creeríais. Atacar naves en llamas "
+    "más allá de Orión. He visto Rayos-C brillar en la oscuridad cerca de "
+    "la puerta de Tannhäuser. Todos esos momentos se perderán en el "
+    "tiempo... como lágrimas en la lluvia. Es hora de morir. 🔫",
+    ("python", "pitón", "piton"): THE_ZEN_OF_PYTHON,
+}
+
+MAXLEN_FOR_USERNAME_TO_TREAT_AS_HUMAN = 100
+
+CHINESE_CHARS_MAX_PERCENT = 0.15
+
+REQUESTS_DEFAULT_TIMEOUT = 30
